a1e90e3871523810740e7153b91814c2d11735e5
Rafael Lage Tavares <rafael.tavares@xing.com>
2020-03-05 19:56:36 +0000

    Updates

--- _next/static/development/pages/index.js
+++ _next/static/development/pages/index.js
@@ -1,5 +1,50 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
 
+/***/ "./components/Basic/Basic.js":
+/*!***********************************!*\
+  !*** ./components/Basic/Basic.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/rafael.tavares/Documents/vizzuality/vizzuality-challenge/components/Basic/Basic.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Basic = function Basic() {
+  return __jsx("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, " Basic ");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Basic);
+
+/***/ }),
+
+/***/ "./components/Basic/index.js":
+/*!***********************************!*\
+  !*** ./components/Basic/index.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _Basic__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Basic */ "./components/Basic/Basic.js");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "default", function() { return _Basic__WEBPACK_IMPORTED_MODULE_0__["default"]; });
+
+
+
+/***/ }),
+
 /***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Frafael.tavares%2FDocuments%2Fvizzuality%2Fvizzuality-challenge%2Fpages%2Findex.js!./":
 /*!*********************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Frafael.tavares%2FDocuments%2Fvizzuality%2Fvizzuality-challenge%2Fpages%2Findex.js ***!
@@ -45,24 +90,32 @@ module.exports = (__webpack_require__(/*! dll-reference dll_82519ec661270f7f484f
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Basic__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Basic */ "./components/Basic/index.js");
 var _jsxFileName = "/Users/rafael.tavares/Documents/vizzuality/vizzuality-challenge/pages/index.js";
 
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
 
 var Index = function Index() {
   return __jsx("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 3
+      lineNumber: 5
     },
     __self: this
   }, __jsx("p", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 4
+      lineNumber: 6
     },
     __self: this
-  }, "Hello Next.js"));
+  }, "Hello Next.js"), __jsx(_components_Basic__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Index);
